b8be9f381d1a96ed87d757412ebc77f93127affe
ayudewita <dewita008@gmail.com>
2020-10-09 08:49:48 +0700

    Updates

---
+++ precache-manifest.2c38169bd440499a0383a49c2c1fa5ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7256e91ee5da1c5a4b8ed086311ca31b",
+    "url": "/doux/index.html"
+  },
+  {
+    "revision": "37dc0638c078f89c76b2",
+    "url": "/doux/static/css/main.5786d015.chunk.css"
+  },
+  {
+    "revision": "8048c7f634e4c76d0d06",
+    "url": "/doux/static/js/2.7c6387cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/doux/static/js/2.7c6387cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37dc0638c078f89c76b2",
+    "url": "/doux/static/js/main.54d74259.chunk.js"
+  },
+  {
+    "revision": "0f2f807ae76d9cc34725",
+    "url": "/doux/static/js/runtime-main.fe40416b.js"
+  },
+  {
+    "revision": "f20397ca677e0ab95b099d123da1cc04",
+    "url": "/doux/static/media/cheesecake.f20397ca.jpg"
+  },
+  {
+    "revision": "e7182b9226f44dd37e3bd6e91ec12152",
+    "url": "/doux/static/media/slice.e7182b92.jpg"
+  }
+]);
